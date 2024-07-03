545f2611c80ad09829a6caa0e9ce1044b0e0c0d2
mohadli <mohammad.ali.real@gmail.com>
2024-07-03 13:19:02 +0330

    image 1

--- api/api.js
+++ api/api.js
@@ -1,5 +1,7 @@
 const { createCanvas } = require('canvas');
 const axios = require('axios');
+const fs = require('fs');
+const bcimage = "./bc.png";
 
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
@@ -16,7 +18,13 @@ export default async (req, res) => {
     return res.status(400).json({ error: 'Missing "name" query parameter' });
   }
 
-  const fileName = `${domainName.replace(/[^a-zA-Z0-9]/g, '')}.png`;
+  const domainABI = [{"inputs": [{"internalType": "string","name": "baseURI","type": "string"}],"stateMutability": "nonpayable","type": "constructor"},{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "owner","type": "address"},{"indexed": true,"internalType": "address","name": "approved","type": "address"},{"indexed": true,"internalType": "uint256","name": "tokenId","type": "uint256"}],"name": "Approval","type": "event"},{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "owner","type": "address"},{"indexed": true,"internalType": "address","name": "operator","type": "address"},{"indexed": false,"internalType": "bool","name": "approved","type": "bool"}],"name": "ApprovalForAll","type": "event"},{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "previousOwner","type": "address"},{"indexed": true,"internalType": "address","name": "newOwner","type": "address"}],"name": "OwnershipTransferred","type": "event"},{"anonymous": false,"inputs": [{"indexed": true,"internalType": "address","name": "from","type": "address"},{"indexed": true,"internalType": "address","name": "to","type": "address"},{"indexed": true,"internalType": "uint256","name": "tokenId","type": "uint256"}],"name": "Transfer","type": "event"},{"inputs": [{"internalType": "string","name": "","type": "string"}],"name": "addrOf","outputs": [{"internalType": "uint256","name": "id","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "to","type": "address"},{"internalType": "uint256","name": "tokenId","type": "uint256"}],"name": "approve","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "address","name": "owner","type": "address"}],"name": "balanceOf","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "tokenId","type": "uint256"}],"name": "getApproved","outputs": [{"internalType": "address","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "owner","type": "address"},{"internalType": "address","name": "operator","type": "address"}],"name": "isApprovedForAll","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "subDomainOwner","type": "address"},{"internalType": "string","name": "subDomain","type": "string"}],"name": "mintSubdomain","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "nonpayable","type": "function"},{"inputs": [],"name": "name","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "","type": "uint256"}],"name": "nameOf","outputs": [{"internalType": "string","name": "ensname","type": "string"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "owner","outputs": [{"internalType": "address","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "tokenId","type": "uint256"}],"name": "ownerOf","outputs": [{"internalType": "address","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "renounceOwnership","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "string","name": "subDomain","type": "string"}],"name": "resolve","outputs": [{"internalType": "address","name": "","type": "address"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "from","type": "address"},{"internalType": "address","name": "to","type": "address"},{"internalType": "uint256","name": "tokenId","type": "uint256"}],"name": "safeTransferFrom","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "address","name": "from","type": "address"},{"internalType": "address","name": "to","type": "address"},{"internalType": "uint256","name": "tokenId","type": "uint256"},{"internalType": "bytes","name": "_data","type": "bytes"}],"name": "safeTransferFrom","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "address","name": "operator","type": "address"},{"internalType": "bool","name": "approved","type": "bool"}],"name": "setApprovalForAll","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "string","name": "baseURI","type": "string"}],"name": "setBaseTokenURI","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "bytes4","name": "interfaceId","type": "bytes4"}],"name": "supportsInterface","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "symbol","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "string","name": "str","type": "string"}],"name": "testAlphaNumeric","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "uint256","name": "tokenId","type": "uint256"}],"name": "tokenURI","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "view","type": "function"},{"inputs": [],"name": "totalSupply","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "from","type": "address"},{"internalType": "address","name": "to","type": "address"},{"internalType": "uint256","name": "tokenId","type": "uint256"}],"name": "transferFrom","outputs": [],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "address","name": "newOwner","type": "address"}],"name": "transferOwnership","outputs": [],"stateMutability": "nonpayable","type": "function"}];
+  const contractAddress = '0x39039e8994e1e58Fc4a56C59ef1B497EeBBA7811';  
+  const infuraProjectId = '8419c212fd48402fb1c28c9fed25e119'; // Replace with your Infura Project ID
+  const provider = new WebSocketProvider(`wss://sepolia.infura.io/ws/v3/${infuraProjectId}`);
+  const contract = new ethers.Contract(contractAddress, domainABI, provider);
+
+  const fileName = `${domainName}.png`;
   console.log(fileName);
 /*
   try {
@@ -38,29 +46,40 @@ export default async (req, res) => {
   }
   */
   console.log("cc");
-  // Generate new image
-  const canvas = createCanvas(800, 200);
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = '#000';
-  ctx.font = '48px Arial';
-  ctx.fillText(domainName, 50, 100);
 
-  const buffer = canvas.toBuffer('image/png');
+  async function generateImageWithText(subdomain, bcimage) {
+    const backgroundImage = await loadImage(bcimage);
+    const canvas = createCanvas(backgroundImage.width, backgroundImage.height);
+    const ctx = canvas.getContext('2d');
+  
+    ctx.drawImage(backgroundImage, 0, 0);
+    ctx.font = 'bold 30px Arial';
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+  
+    const textY = canvas.height * 0.75;
+    ctx.fillText(`Token ID: ${subdomain}`, canvas.width / 2, textY);
+    const outputBuffer = canvas.toBuffer('image/png');
+    if (outputBuffer) console.log(`Image on Buffer`);
+  }
+
+  generateImageWithText("first", bcimage);
+  console.log("OOO");
 
   // Upload the image to Cloudinary
   const result = await cloudinary.uploader.upload_stream(
     { public_id: fileName, resource_type: 'image' },
     (error, result) => {
       if (error) {
+        console.log("aaa");
         return res.status(500).json({ error: 'Failed to upload image' });
       }
+      console.log("UUU");
       generateResponseJson(domainName, result.secure_url, res);
     }
   );
-
-  buffer.pipe(result);
+  console.log("YYY");
+  outputBuffer.pipe(result);
 };
 
 const generateResponseJson = (domainName, imageUrl, res) => {
