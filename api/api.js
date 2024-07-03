7cdc57008de8102169fb4a2d2e5d4386c4755127
mohadli <mohammad.ali.real@gmail.com>
2024-07-03 16:06:05 +0330

    cld 1

--- api/api.js
+++ api/api.js
@@ -11,7 +11,9 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-console.log(cloud_name,api_key,api_secret);
+console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY);
+console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET);
 
 export default async (req, res) => {
   const { name: domainName } = req.query;
